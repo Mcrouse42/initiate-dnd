e8f93169ead5df8a4bcde9c1a3d90dc25e638722
Rae Stichter <raestichter@gmail.com>
2021-04-05 01:00:40 -0400

    dungeon master and player backend routes for get complete and tested

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,43 +3,100 @@ const { DungeonMaster, Player, Monster } = require('../models');
 const resolvers = {
   Query: {
     // get all dungeon masters
-    // to test:
-    // query {
-    //   dungeonMasters {
-    //     _id
-    //     username
-    //     email
-    //   }
-    // }
     dungeonMasters: async () => {
-      return DungeonMaster.find();
+      return DungeonMaster.find()
+        .select('-__v -password')
+        .populate('players');
     },
 
     // get a dungeon master by username
-    // to test:
-    // query {
-    //   dungeonMaster(username: "Amira99") {
-    //     _id
-    //     username
-    //     email
-    //   }
-    // }
     dungeonMaster: async (parent, { username }) => {
       const params = username ? { username } : {};
-      return DungeonMaster.find(params);
+      return DungeonMaster.find(params)
+        .select('-__v -password')
+        .populate('players');
     },
 
     // get all players
-    // players: async () => {
-    //   return Player.find();
-    // },
+    players: async () => {
+      return Player.find();
+    },
 
     // get player by playerName
-    // player: async (parent, { playerName }) => {
-    //   const params = playerName ? { playerName } : {};
-    //   return Player.find(params);
-    // }
+    player: async (parent, { playerName }) => {
+      const params = playerName ? { playerName } : {};
+      return Player.find(params);
+    }
   }
 };
 
 module.exports = resolvers;
+
+// ------------------------ BACKEND TESTING INFO ------------------------
+// get all dungeon masters (players included):
+
+// query {
+//   dungeonMasters {
+//     _id
+//     username
+//     email
+//     players {
+//       _id
+//       playerName
+//   		playerClass
+//   		playerRace
+//   		playerLevel
+//   		playerArmorClass
+//   		playerHitPoints
+//     }
+//   }
+// }
+
+
+// get one dungeon master by username (players included)
+
+// query {
+//   dungeonMaster(username: "Leopold.Batz95") {
+//     _id
+//     username
+//     email
+//     players {
+//       _id
+//       playerName
+//   		playerClass
+//   		playerRace
+//   		playerLevel
+//   		playerArmorClass
+//   		playerHitPoints
+//     }
+//   }
+// }
+
+
+// get all players
+
+// query {
+//   players {
+//     _id
+//   	playerName
+//   	playerClass
+//   	playerRace
+//   	playerLevel
+//   	playerArmorClass
+//   	playerHitPoints
+//   }
+// }
+
+
+// get one player by playerName
+// query {
+//   player(playerName: "Kacey_Botsford") {
+//     _id
+//     playerName
+//   	playerClass
+//   	playerRace
+//   	playerLevel
+//   	playerArmorClass
+//   	playerHitPoints
+//   }
+// }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,3 @@
-// Initiate dnd
-
-// players
-// DM
-// Monsters
-
-// DM can have many players
-
-
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
@@ -26,22 +17,13 @@ type Player {
   playerLevel: Int
   playerArmorClass: Int
   playerHitPoints: Int
-
 }
 
 type Query {
     dungeonMasters: [DungeonMaster]
     dungeonMaster(username: String): [DungeonMaster]
+    players: [Player]
+    player(playerName: String): [Player]
   }
 `;
 module.exports = typeDefs;
-
-// thoughts: [Thought]
-
-// ------ This is the query code if you want to check for all dm,dm by username, all players, player by playername
-// type Query {
-//   dungeonMasters: [DungeonMaster]
-//   dungeonMaster(username: String): [DungeonMaster]
-//   players: [Player]
-//   player(playerName: String): [Player]
-// }
