a8ba971586815499c75858ab3eaa57e2dd0ad310
Zoe King <zoeking1221@gmail.com>
2021-04-14 12:27:21 -0400

    trying to get initiative sort to work

--- client/src/components/BattlePlayerList/index.js
+++ client/src/components/BattlePlayerList/index.js
@@ -1,73 +1,92 @@
-import React, { Component } from "react";
-import Modal from '../Modal';
+import React, { useState, useEffect } from "react";
+import Modal from "../Modal";
 
+const BattlePlayerList = (props) => {
+    const [modelStatus, setModalStatus] = useState({ show: false });
+    const [playersArray, setPlayersArray] = useState(props.players);
 
-class BattlePlayerList extends Component { 
-    constructor() {
-        super();
-        this.state = {
-            show: false
-        };
-        this.showModal = this.showModal.bind(this);
-        this.hideModal = this.hideModal.bind(this);
+    let givePlayersInit = () => {
+        let playerInitMap = playersArray.map(player => {
+            player.initiative = 0;
+            return player
+        });
+
+        setPlayersArray(playerInitMap);
     }
 
-    showModal = () => {
-        this.setState({ show: true });
-    };
+    useEffect(() => {
+        givePlayersInit();
+    }, []);
 
-    hideModal = () => {
-        this.setState({ show: false });
-    };
+  let showModal = () => {
+    setModalStatus({ show: true });
+  };
 
-    render() {
-        return (
-            <div>
-                
-                {this.props.players.map((player) =>
-                    <div key={player._id} className="card mb-3">
-                        <p className="card-header">
-                            <h2>{player.playerName}<span onClick={this.showModal}> + </span></h2>
-                            <textarea> Test</textarea>
-                            <Modal show={this.state.show} handleClose={this.hideModal}>
-                                <h2>{player.playerName}</h2>
-                                <p>Player Race: {player.playerRace}</p>
-                                <p>Player Class: {player.playerClass}</p>
-                                <p>Player Level: {player.playerLevel}</p>
-                                <p>Player AC: {player.playerArmorClass}</p>
-                                <p>Player HP: {player.playerHitPoints}</p>
-                                <p>Player STR: {player.playerStrengthStat}</p>
-                                <p>Player DEX: {player.playerDexterityStat}</p>
-                                <p>Player CON: {player.playerConstitutionStat}</p>
-                                <p>Player INT: {player.playerIntelligenceStat}</p>
-                                <p>Player WIS: {player.playerWisdomStat}</p>
-                                <p>Player CHA: {player.playerCharismaStat}</p>
-                            </Modal>
-                            {/* <button type="button" onClick={this.showModal}>
-                                Open
-                            </button> */}
+  let hideModal = () => {
+      setModalStatus({ show: false });
+  };
 
-                        </p>
-                        <div className="card-body">          
-                            <div className="card mb-3">
-                                <p>Player AC: {player.playerArmorClass}</p>
-                                <form>
-                                    <label htmlFor="playerArmorClass">Player AC:</label>
-                                    <input
-                                        id="playerArmorClass"
-                                        placeholder={player.playerArmorClass}
-                                        input="number"
-                                       />
-                                </form>
-                                <p>Player HP: {player.playerHitPoints}</p>
-                                {/* <textarea id="initiative">Initiative</textarea> */}
-                            </div>
-                        </div>
-                    </div>
-                )}
+  // ZK ATTEMPTS:
+
+//   let mySortFunction = (playersArray) => {
+//     let a = a.player;
+//     let b = b.player;
+//     playersArray.sort((a, b) => (b.initiative - a.initiative));
+//     console.log('my SortFunction');
+// };
+
+const mySortFunction = () => {
+        console.log(document.getElementById('initiative').value);
+        let initiatve = document.getElementById('initiative').value;
+        playersArray.sort(function(a, b) {
+        return a.initiative - b.initiative;
+    });
+};
+
+
+return (
+    <div>
+      {playersArray.map((player) => (
+        <div key={player._id} className="card mb-3">
+          <p className="card-header">
+            <h2>
+              {player.playerName}
+              <span onClick={showModal}> + </span>
+                      <input id="initiative" onChange={mySortFunction} placeholder="initiative number" />
+            </h2>
+            <Modal show={modelStatus.show} handleClose={hideModal}>
+              <h2>{player.playerName}</h2>
+              <p>Player Race: {player.playerRace}</p>
+              <p>Player Class: {player.playerClass}</p>
+              <p>Player Level: {player.playerLevel}</p>
+              <p>Player AC: {player.playerArmorClass}</p>
+              <p>Player HP: {player.playerHitPoints}</p>
+              <p>Player STR: {player.playerStrengthStat}</p>
+              <p>Player DEX: {player.playerDexterityStat}</p>
+              <p>Player CON: {player.playerConstitutionStat}</p>
+              <p>Player INT: {player.playerIntelligenceStat}</p>
+              <p>Player WIS: {player.playerWisdomStat}</p>
+              <p>Player CHA: {player.playerCharismaStat}</p>
+            </Modal>
+          </p>
+          <div className="card-body">
+            <div className="card mb-3">
+              <p>Player AC: {player.playerArmorClass}</p>
+              <form>
+                <label htmlFor="playerHitPoints">Player HP:</label>
+                <input
+                  id="playerHitPoints"
+                  placeholder={player.playerHitPoints}
+                  input="number"
+                />
+              </form>
             </div>
-        );
-    }
-}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
 
 export default BattlePlayerList;
